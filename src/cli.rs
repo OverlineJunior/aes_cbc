6cf4595c5c50dc76a2f4cd5bfe315c277d8321eb
over <ov3rline@proton.me>
2025-11-16 17:27:30 -0300

    Simplify implementation by removing hex conversion

--- src/cli.rs
+++ src/cli.rs
@@ -1,80 +1,80 @@
-use std::{fs, io, path::Path};
-use cliclack::*;
 use crate::crypto::Content;
+use cliclack::*;
+use std::{fs, io, path::Path};
 
 #[derive(Clone, PartialEq, Eq)]
 enum ActionKind {
-	Encrypt,
-	Decrypt,
+    Encrypt,
+    Decrypt,
 }
 
 pub enum Action {
-	Encrypt { raw_input: Vec<u8> },
-	Decrypt { raw_cipher: Vec<u8>, raw_key: Vec<u8> },
+    Encrypt { input: Vec<u8> },
+    Decrypt { cipher: Vec<u8>, key: Vec<u8> },
 }
 
 pub fn run_cli(handler: fn(Action)) -> io::Result<()> {
-	clear_screen()?;
+    clear_screen()?;
 
-	intro("AES Crypto CLI")?;
+    intro("AES Crypto CLI")?;
 
-	let action = read_action()?;
+    let action = read_action()?;
 
-	let (pending_msg, done_msg) = match action {
-		Action::Encrypt { .. } => ("Encrypting...", "Encryption complete!"),
-		Action::Decrypt { .. } => ("Decrypting...", "Decryption complete!"),
-	};
+    let (pending_msg, done_msg) = match action {
+        Action::Encrypt { .. } => ("Encrypting...", "Encryption complete!"),
+        Action::Decrypt { .. } => ("Decrypting...", "Decryption complete!"),
+    };
 
-	let spin = spinner();
-	spin.start(pending_msg);
+    let spin = spinner();
+    spin.start(pending_msg);
 
-	handler(action);
+    handler(action);
 
-	spin.stop("");
+    spin.stop("");
 
-	outro(done_msg)?;
+    outro(done_msg)?;
 
-	Ok(())
+    Ok(())
 }
 
 fn read_action() -> io::Result<Action> {
-	let action_kind = select("Select an action:")
-		.item(ActionKind::Encrypt, "Encrypt", "")
-		.item(ActionKind::Decrypt, "Decrypt", "")
-		.interact()?;
+    let action_kind = select("Select an action:")
+        .item(ActionKind::Encrypt, "Encrypt", "")
+        .item(ActionKind::Decrypt, "Decrypt", "")
+        .interact()?;
 
-	let action = match action_kind {
-		ActionKind::Encrypt => Action::Encrypt {
-			raw_input: read_file("input")?
-		},
-		ActionKind::Decrypt => Action::Decrypt {
-			raw_cipher: read_file("encrypted")?,
-			raw_key: read_file("key")?,
-		},
-	};
+    let action = match action_kind {
+        ActionKind::Encrypt => Action::Encrypt {
+            input: read_file("input")?,
+        },
+        ActionKind::Decrypt => Action::Decrypt {
+            cipher: read_file("encrypted")?,
+            key: read_file("key")?,
+        },
+    };
 
-	Ok(action)
+    Ok(action)
 }
 
 fn read_file(adjective: &str) -> io::Result<Content> {
-	let path: String = input(format!("Enter the path to the {adjective} file:"))
-		.placeholder(&format!("Path to {adjective} file"))
-		.validate(|input: &String| {
-			let path = Path::new(input);
-			match (path.exists(), path.is_file()) {
-				(false, _) => Err("File does not exist"),
-				(_, false) => Err("Path is not a file"),
-				(true, true) => Ok(()),
-			}
-		})
-		.interact()?;
+    let path: String = input(format!("Enter the path to the {adjective} file:"))
+        .placeholder(&format!("Path to {adjective} file"))
+        .validate(|input: &String| {
+            let path = Path::new(input);
+            match (path.exists(), path.is_file()) {
+                (false, _) => Err("File does not exist"),
+                (_, false) => Err("Path is not a file"),
+                (true, true) => Ok(()),
+            }
+        })
+        .interact()?;
 
-	fs::read(path)
+    fs::read(path)
 }
 
 // fn read_decryption_key() -> io::Result<KeyString> {
 // 	let key_str: String = input("Enter the decryption key:")
-// 		.placeholder(&format!("{} characters key", raw_key))
+// 		.placeholder(&format!("{} characters key", key))
 // 		.interact()?;
 
 // 	Ok(key_str)
--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,13 @@ use crate::{cli::{Action, run_cli}, crypto::{decrypt, encrypt}};
 
 fn execute_action(action: Action) {
     match action {
-        Action::Encrypt { raw_input } => {
-            let (cipher, key_hex) = {
-                let (c, k) = encrypt(&raw_input);
-                (c, hex::encode(k))
-            };
-
+        Action::Encrypt { input } => {
+            let (cipher, key) = encrypt(&input);
             fs::write("encrypted.bin", &cipher).expect("Failed to write cipher to file");
-            fs::write("key.hex", &key_hex).expect("Failed to write key to file");
+            fs::write("key.hex", key).expect("Failed to write key to file");
         }
-        Action::Decrypt { raw_cipher, raw_key } => {
-            let key: crypto::Key = hex::decode(raw_key)
-                .expect("Invalid hex in decryption key")
-                .as_slice()
-                .try_into()
-                .expect("Invalid key length");
-
-            let decrypted = decrypt(&raw_cipher, &key);
+        Action::Decrypt { cipher, key } => {
+            let decrypted = decrypt(&cipher, &key.try_into().expect("Key does not fit"));
             fs::write("decrypted.txt", &decrypted).expect("Failed to write decrypted content to file");
         }
     }
